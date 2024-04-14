60062de457c7bba773b068f9986e458a3674be97
MuhammadZayne358 <167001323+MuhammadZayne358@users.noreply.github.com>
2024-04-14 17:39:52 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cli-Number-Guessing358
+This is a simple Cli number guessing game
---
+++ index.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+// 1) Computer will generate a random number
+// 2) User will input a random number 
+// 3) Compare user input with computer random number and show result
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userguessnumber",
+        type: "number",
+        message: "Please guess a number between 1 to 10: ",
+    },
+]);
+if (answers.userguessnumber === randomNumber) {
+    console.log("Congrutalions! you guessed the right number.");
+}
+else {
+    console.log("Alas you guess the wrong number");
+}
